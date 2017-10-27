eb726f27d58192999ebd678b2d8939cb89a48797
朱逢源 <zhufyaxel@gmail.com>
2017-10-27 19:06:55 -0400

    First push
    Add PerlinNoise

---
+++ Assets/scripts/mapGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapGenerator : MonoBehaviour {
+	public int mapWidth;
+	public int mapHeight;
+	public float scale;
+
+	public void GenerateMap(){
+		float [,] noiseMap = Noise.GeneratorNoiseMap( mapWidth, mapHeight, scale);
+	}
+}
